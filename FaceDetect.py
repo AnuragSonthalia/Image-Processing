2ade6dab3e8874544574b325430599d69c73fa50
AnuragSonthalia <anuragsonthalia97@gmail.com>
2017-05-25 12:02:22 +0530

    Initial commit

---
+++ FaceDetect.py
@@ -0,0 +1,9 @@
+import cv2
+
+class FaceDetector:
+    def __init__(self, faceCascadePath):
+        self.faceCascade = cv2.CascadeClassifier(faceCascadePath)
+
+    def detect(self, image, scalefactor=1.3, minneighbors=5, minsize=(30, 30)):
+        rects = self.faceCascade.detectMultiScale(image, scaleFactor=scalefactor, minNeighbors=minneighbors,minSize=minsize, flags = cv2.CASCADE_SCALE_IMAGE)
+        return rects
